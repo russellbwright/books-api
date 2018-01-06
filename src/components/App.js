e54b564ff43d4d6e55cdcd998046f3a0ba23b68c
Russell B W <russell.bwright@yahoo.com>
2018-01-06 01:52:29 -0600

    messing around

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './Home/Home'
+import Single from './Single/Single'
+
 
 import '../App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Route exact path='/' component={Home} />
+          <Route path='/api/:single_name' component={Single} />
+        </div>
+      </BrowserRouter>  
     );
   }
 }
---
+++ src/components/Single/Single.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class Single extends Component {
+    constructor(props){
+        super(props)
+
+            this.state={
+                person: []
+            }
+    }
+
+
+    render() {
+        return(
+            <div>
+                Hey
+            </div>    
+        )
+    }
+}
+
+export default Single
